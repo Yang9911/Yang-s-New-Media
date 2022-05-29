884415a99ce6d21865aadb43b892c9942cf96dfd
Chia-Hao Yang <yang9911@Gmail.com>
2022-05-29 21:35:36 +0800

    Initial Commit

---
+++ Yang's New Media/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yang's New Media
+//
+//  Created by Chia-Hao Yang on 2022/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yang's New Media/Yang_s_New_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yang_s_New_MediaApp.swift
+//  Yang's New Media
+//
+//  Created by Chia-Hao Yang on 2022/5/29.
+//
+
+import SwiftUI
+
+@main
+struct Yang_s_New_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
